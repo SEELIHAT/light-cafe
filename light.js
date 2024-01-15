87f78335b3b3861c28c7c95a5d98071c713e1a3f
SEELIHAT <etumanov77@gmail.com>
2024-01-15 01:33:01 -0800

    Initial commit

---
+++ light.js
@@ -0,0 +1,53 @@
+function toggleRedClass(element) {
+    let opaElement = document.querySelector('.twoaside');
+     element.classList.toggle('red');
+     opaElement.style.opacity = "1";
+ }
+ 
+ function changeOpacity() {
+             var opaElement = document.querySelector(".twoaside");
+             if (opaElement) {
+                 opaElement.style.opacity = "0";
+             }
+         }
+ 
+         function addToMessageAndSend(event) {
+     event.preventDefault();
+     var redButtons = document.querySelectorAll('.red');
+     var textarea = document.getElementById('addressPhone');
+     var email = 'hayatihan70@gmail.com';
+     // Check if red buttons are selected
+     if (redButtons.length > 0) {
+         redButtons.forEach(function (redButton) {
+             var buttonText = redButton.textContent.trim();
+             textarea.value += '\n'  + buttonText ;
+         });
+ 
+         // Create a mailto link to open the user's default email client
+         var emailLink = 'mailto:' + email + '?subject=Order&body=' + encodeURIComponent(textarea.value);
+ 
+         // Open the email client
+         window.location.href = emailLink;
+ 
+         // Clear the textarea after sending
+         textarea.value = '';
+         textarea.value += '\nwe will call you.If want again order refresh page';
+         textarea.style.color = 'red';
+         
+         textarea.style.textAlign = 'center';
+         // Display a thank you message
+         alert('Thank you for the order!');
+     } else {
+         // If no red buttons are selected, show an alert
+         alert('Please select at least one item before sending the order.');
+     }
+ }
+
+
+
+
+
+
+
+
+
