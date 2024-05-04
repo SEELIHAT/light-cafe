f08e88c77ae2b0a02ce28579e46fbd8ccd0fe109
SEELIHAT <etumanov77@gmail.com>
2024-05-03 19:53:44 -0700

    js close

--- light.js
+++ light.js
@@ -44,7 +44,12 @@ function toggleRedClass(element) {
  }
 
 
-
+ let hay = prompt('WE DON\'t WORK THIS and NEXT MOUNTH SORRY','All right?');
+ if (hay === 'Ok') {
+     alert('thank you');
+ }
+ 
+ 
 
 
 
